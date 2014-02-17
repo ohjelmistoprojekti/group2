534e08962793d76ad4a00cc9ae86b9790e693324
Niko Kaukonen <centria.otek@gmail.com>
2014-02-17 12:03:09 +0200

    Added harkat

---
+++ harjoitukset/nkaukonen/validation.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+</head>
+<body>
+<?PHP
+class userValidation {
+	private $defaultLogin = 'etunimi.sukunimi@abc.fi'; // "tietokannassa"
+	private $defaultPassword = 'pellepeloton112'; //"tietokannassa"
+
+	public function Validate() {
+	$username = $_POST['username'];
+			$salt = 'abg4';
+					$pw_hashed = hash('sha256' ,$salt.$defaultPassword);					
+	
+		if (filter_var($username, FILTER_VALIDATE_EMAIL) && 
+			($username == $this->defaultLogin) && ($username !=NULL) &&($pw_hashed == hash('sha256', $salt.$_POST['password'])) {
+				echo 'Well done, Viljami.';
+			else
+				echo 'Ketuiks män.';
+		}
+	}
+}
+?>
+</body>
+</html>
