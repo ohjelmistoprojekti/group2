1f55fb00d54b114911eba4c028792402d8a699bb
Michal Iwanski <michal.iwanski@cou.fi>
2014-03-05 14:50:12 +0200

    miwanski made assignment 2

--- public_html/harjoitukset/miwanski/login.php
+++ public_html/harjoitukset/miwanski/login.php
@@ -1,35 +1,15 @@
+<?
+	session_start();
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<?php
-	include 'emailChecker.php';
-	include 'validator.php';
-	if($_POST != null)
-	{
-		$userdata['username'] = $_POST['username'];
-		if(emailCheck($userdata['username']) == 0)
-		{
-			$notemail = '<br><font color = "red" >This is not a valid email address.</font>';
-		}
-		else
-		{
-			$userdata['password'] = $_POST['password'];
-			if(validator($userdata['username'], $userdata['password']) == 1)
-			{
-				$loginsuccess 		= '<br><font color = "green" >Login Success!</font>';
-			}
-			else
-			{
-				$invalidcredentials = '<br><font color = "red" >Wrong username or password</font>';
-			}
-		}
-	}
-?>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style.css">
-<body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
 
-<!------ <form id='login' action='mysql.php' method='post' accept-charset='UTF-8'>
 <fieldset >
 <legend>Login</legend>
 <input type='hidden' name='submitted' id='submitted' value='1'/>
@@ -45,22 +25,26 @@
 </form>
 -->
 
-<div id="container">       
-<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
-<label for="username">Username:</label>
-<input type="text" id="username" name="username">
-<label for="password">Password:</label>
-<input type="password" id="password" name="password">
-<div id="lower">
-<input type="submit" value="Login">
-</div>
-</form>
-<?php
-print $notemail; 
-print $invalidcredentials;
-print $loginsuccess;
-?>
-</div>
+	<div id="container">       
+		<form id='login' action='login_check.php' method='post' accept-charset='UTF-8'>
+			<label for="username">Username:</label>
+			<input type="text" id="username" name="username">
+
+			<label for="password">Password:</label>
+			<input type="password" id="password" name="password">
+			<div id="lower">
+			<input type="submit" value="Login">
+			</div>
+		</form>
+		<?php
+			//Just in case if something went wrong
+			if(strlen($_SESSION['login_failed']) > 0)
+			{
+				echo "<font color='red'>".$_SESSION['login_failed']."</font>";;
+			}
+		?>
+	</div>
+
 </body>
 </html>
 
--- public_html/harjoitukset/miwanski/validator.php
+++ public_html/harjoitukset/miwanski/validator.php
@@ -1,30 +1,104 @@
 <?
-	$invalidcredentials;
-	$loginsuccess;
-	function validator($user, $pass)
-	{
-		$con = mysql_connect("10.177.3.206", "miwanski", "doofLLAB2601");
-		$db_select = mysql_select_db("Group2", $con);
-		$username = $user;
-		$pw = $pass;
-		$salt = 'oprj';
-		$pw_hash = sha1($salt.$pw);
-		$sql = "SELECT * FROM members
-				WHERE email = '$username'";
-		$result = mysql_query($sql, $con);
+	session_start();
 
-		$userData = mysql_fetch_array($result, MYSQL_ASSOC);
-		if($pw_hash == $userData['password'])
+	
+	// 	Class: validator
+	//	Handles:	The user login validation process 
+	//				and user authentication and privileges
+	class validator
+	{
+		//	Function:	checkLogin
+		//	Handles:	Checks if the user is logged in. If not, 
+		//				send back to login with error report
+		function checkLogin($priviledges = false)
 		{
-			return 1;
-			// header('Location: home_success.php');
+			if(strlen($_SESSION['username']) == 0)
+			{
+				$_SESSION['login_failed'] = "How the F**K did u get here without login?? Back to your hole, u scum";
+				header('location:login.php');
+			}
+			
+			if($priviledges)
+			{
+				//Check if the user has priviledges to THIS page/function/functionality
+			}
 		}
-		else
+		
+		//	Function:	validate
+		//	Handles:	Login validating, database connection, comparing,
+		//				session data storing and possible error reporting
+		//	Requires:	User given username and password
+		function validate($user, $pass)
 		{
-			return 0;
+			//	Function:	emailCheck
+			//	Handles:	Checks if the user given username is an email
+			//	Requires:	User given email
+			function emailCheck($x)
+			{
+				$email = $x;
+
+				$dots = 0;
+				$atsign = 0;
+
+				for ($k=0; $k < strlen($email); $k++)
+				{
+					if ($email[$k] == '@')
+					{
+						$atsign = 1;
+						break;
+					}
+				}
+
+				for ($k=0; $k < strlen($email); $k++)
+				{
+					if ($email[$k] == '.')
+					{
+					  $dots += 1;
+					  break;
+					}
+				}
+
+				if ($dots < 1 || $atsign != 1)
+				   return 0;
+				else
+				   return 1;
+			}
+			
+			if(!emailCheck($user))
+			{
+				$_SESSION['login_failed'] = "The username is not an email.";
+				header('location:login.php');
+			}
+		
+			//Connects to db
+			$con = mysql_connect("10.177.3.206", "miwanski", "doofLLAB2601");
+			$db_select = mysql_select_db("Group2", $con);
+			$username = $user;
+			$pw = $pass;
+			$salt = 'oprj';
+			$pw_hash = sha1($salt.$pw);
+			$sql = "SELECT * FROM members
+					WHERE email = '$username'";
+			$result = mysql_query($sql, $con);
+
+		
+			
+			$userData = mysql_fetch_array($result, MYSQL_ASSOC);
+			
+			//Login matches
+			if($pw_hash == $userData['password'])
+			{
+				$_SESSION['login_failed'] = null;
+				$_SESSION['username'] = $username;
+				header('location:login_success.php');
+			}
+			//Login Fails
+			else
+			{
+				$_SESSION['login_failed'] = "The username or password is invalid.";				
+				header('location:login.php');
+			}
+			mysql_close($con);
 		}
-		mysql_close($con);
 	}
-	
-	
 ?>
